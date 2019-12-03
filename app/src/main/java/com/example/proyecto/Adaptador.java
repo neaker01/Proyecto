fef7fe8cf3bbb905b4d134f746db45c0b95f3da5
Neaker <snakeone93@hotmail.com>
2019-12-03 20:37:37 +0100

    Creado recycler, registro, login

---
+++ app/src/main/java/com/example/proyecto/Adaptador.java
@@ -0,0 +1,97 @@
+package com.example.proyecto;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder>{
+
+    private static final String TAG = "XYZ";
+    private final OnItemClickListener listener;
+    private ArrayList<Bicicleta> lista;
+
+
+    public Adaptador(ArrayList<Bicicleta> lista, OnItemClickListener listener) {
+        this.lista = lista;
+        this.listener = listener;
+    }
+
+    public void setArray(ArrayList<Bicicleta> lista) {
+        this.lista = lista;
+    }
+
+    @NonNull
+    @Override
+    public Adaptador.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_productos, viewGroup, false);
+        return new Adaptador.ViewHolder(itemView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull Adaptador.ViewHolder viewHolder, int i) {
+
+       /* Test test = (Test) listaTests.get(i);
+
+        viewHolder.txNumTest.setText(test.getId());
+        if (test.getAprobado()){
+            viewHolder.txAprobado.setText("Si"); }
+        else{   viewHolder.txAprobado.setText("No"); }
+
+        viewHolder.txNumAciertos.setText(test.getAciertos());
+        viewHolder.txNumFallos.setText(test.getFallos());
+
+    */
+
+        viewHolder.bind(lista.get(i), (OnItemClickListener) listener);
+    }
+
+
+
+    public interface OnItemClickListener {
+        void onItemClick(Bicicleta b);
+
+    }
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        //  ImageView imagenLectura;
+        TextView txModelo;
+        TextView txPrecio;
+        ImageView imagenBike;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //   imagenLectura=itemView.findViewById(R.id.imagenLecturaRecycler);
+            txModelo = itemView.findViewById(R.id.recycler_modelo);
+            txPrecio = itemView.findViewById(R.id.recycler_precio);
+            imagenBike = itemView.findViewById(R.id.recycler_image);
+
+        }
+
+        public void bind(final Bicicleta b, final OnItemClickListener listener) {
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    listener.onItemClick(b);
+                }
+            });
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/proyecto/Login.java
+++ app/src/main/java/com/example/proyecto/Login.java
@@ -1,14 +1,116 @@
 package com.example.proyecto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity {
+
+    private TextView user;
+    private TextView pass;
+    private Button btLogin;
+    private FirebaseAuth autentificador;
+    private FirebaseUser fbUser;
+    FirebaseDatabase firebaseDatabase;
+    String usuario;
+    String clave;
+    Preferencias preferencias;
+
+    String prefEmail;
+    String prefPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseDatabase= FirebaseDatabase.getInstance();
+        FirebaseApp.initializeApp(this);
+        autentificador =  FirebaseAuth.getInstance();
+
+        preferencias = new Preferencias(getApplicationContext());
+        user=(TextView)findViewById(R.id.txUserLogin);
+        pass=(TextView)findViewById(R.id.txPassLogin);
+        btLogin = (Button) findViewById(R.id.btLogin);
+        usuario = user.getText().toString();
+        clave = pass.getText().toString();
+
+
+        if (preferencias.getSesion() != null){
+            String sesion =preferencias.getSesion();
+            String[] arraySesion = sesion.split("-");
+            try {
+                prefEmail = arraySesion[0];
+                prefPassword = arraySesion[1];
+                iniciarSesion(prefEmail, prefPassword);
+            }catch (ArrayIndexOutOfBoundsException ex){
+                // Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!user.getText().toString().isEmpty() &&
+                        !pass.getText().toString().isEmpty()){
+                    // When a user signs in to your app, pass the user's email address and password to signInWithEmailAndPassword
+                    autentificador.signInWithEmailAndPassword(user.getText().toString(), pass.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        fbUser = autentificador.getCurrentUser();
+                                        preferencias.guardarPreferencias(user.getText().toString(), pass.getText().toString());
+                                        Intent i = new Intent(Login.this, MainActivity.class);
+                                        startActivity(i);
+                                    } else {
+                                        Toast.makeText(Login.this, "Fallo autentificacion", Toast.LENGTH_SHORT).show();
+
+                                        try{
+                                            preferencias.eliminarPreferencias();
+                                        } catch (NullPointerException ex){
+                                        }
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
+
+
+    public void iniciarSesion(String email, String password){
+        final String finalEmail = email;
+        final String  finalPass=password;
+        autentificador.signInWithEmailAndPassword(email, password).addOnCompleteListener
+                (new OnCompleteListener<AuthResult>() {
+                    private static final String TAG = "MITAG";
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            fbUser= autentificador.getCurrentUser();
+                            Intent i = new Intent(Login.this, MainActivity.class);
+                            startActivity(i);
+                        }
+                        else{
+                            preferencias.eliminarPreferencias();
+                            Log.v(TAG, "ERROOOOOO "+task.getException().toString() );}}});}
 }
+
+
